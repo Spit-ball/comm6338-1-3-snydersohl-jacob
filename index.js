845fac7fb798600b8af2706cd18a8a0d3a62161a
Ian Snyder <83725076+Spit-ball@users.noreply.github.com>
2023-01-25 16:14:24 -0600

    adjusted alert statement to eliminate some text and fit the mocha testing. the end result is the same as prior, but automated test now passes.

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 // Your code here
 var trueOrFalseString = 'True or False: '
 
-var questionsArr= [
-    { question: trueOrFalseString + "Jump rope is a form of cardiovascular exercise.", answer: true},         
-    { question: trueOrFalseString + 'Shoelaces keep your feet warm.', answer: false},
-    { question: trueOrFalseString + 'The Xbox is made by Sony.', answer: false},
-    { question: trueOrFalseString + 'Vodka is traditionally made with potatoes.', answer: true},
-    { question: trueOrFalseString + 'Pikachu is yellow.', answer: true}
+var questionsArr = [
+    { question: trueOrFalseString + "Jump rope is a form of cardiovascular exercise.", answer: true },
+    { question: trueOrFalseString + 'Shoelaces keep your feet warm.', answer: false },
+    { question: trueOrFalseString + 'The Xbox is made by Sony.', answer: false },
+    { question: trueOrFalseString + 'Vodka is traditionally made with potatoes.', answer: true },
+    { question: trueOrFalseString + 'Pikachu is yellow.', answer: true }
 ]
 
-console.log(questionsArr)
+console.log(questionsArr);
 
 function runQuiz() {
     var score = 0
-    for(var i=0; i < questionsArr.length; i++){
-        var userAnswer = confirm(questionsArr[i].question)
-            if(userAnswer == questionsArr[i].answer) {
-                score ++
+    for (var i = 0; i < questionsArr.length; i++) {
+        var userAnswer = confirm(questionsArr[i].question);
+        if (userAnswer == questionsArr[i].answer) {
+            score++;
         }
     }
-    var percent = (score / questionsArr.length) * 100
-    var roundedPercent = Math.round(percent)
-    alert('You scored a ' + roundedPercent + " out of 100%!")
-    }
+    var percent = (score / questionsArr.length) * 100;
+    var roundedPercent = Math.round(percent);
+    alert("You scored a " + roundedPercent + "%!");
+}
